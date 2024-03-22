7ec3c41c3e4ebd37b0da9a4b6e40344a11f812f3
wgb-code <front.willian@gmail.com>
2024-03-21 21:31:54 -0300

    Refatorando.

---
+++ app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+    "/views/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
